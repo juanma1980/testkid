43d689de88147e4cc9f298ad6584afd80b361d39
juanma1980 <juanma1980@gmail.com>
2019-10-04 08:50:20 +0200

    fix rsrc path

--- src/runoconfig.py
+++ src/runoconfig.py
@@ -6,7 +6,7 @@ from appconfig.appConfigScreen import appConfigScreen as appConfig
 
 app=QApplication(["Run-O-Matic"])
 config=appConfig("Run-O-Matic",{'app':app})
-config.setRsrcPath="/usr/share/runomatic/rsrc"
+config.setRsrcPath="%s/rsrc"%os.curdir
 config.setIcon('runomatic.svg')
 config.setBanner('banner.png')
 config.setBackgroundImage('background.png')
