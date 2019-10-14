0d9e6bffc578ea4d926fd86b019063907b85413d
juanma1980 <juanma1980@gmail.com>
2019-10-14 09:45:55 +0200

    reworked autostart

--- src/runoservice.py
+++ src/runoservice.py
@@ -6,7 +6,6 @@ from appconfig.appConfig import appConfig
 config=appConfig()
 config.set_configFile("runomatic.conf")
 config.set_baseDirs({'system':'/usr/share/runomatic'})
-n4d=True
 data={}
 if os.path.isfile('/usr/share/runomatic/runomatic.conf'):
 	conf=config.getConfig('system')
@@ -16,20 +15,10 @@ if os.path.isfile('/usr/share/runomatic/runomatic.conf'):
 		data=conf['n4d'].copy()
 	elif level=='user':
 		conf=config.getConfig('user')
-		data=conf['n4d'].copy()
+		data=conf['user'].copy()
 	else:
 		data=conf['system'].copy()
 
 startup=data.get('startup','')
 if str(startup).lower()=='true':
-	with open ("/etc/xdg/autostart/runomatic.desktop","w") as f:
-		f.write("[Desktop Entry]\n")
-		f.write("Encoding=UTF-8\n")
-		f.write("Name=runomatic\n")
-		f.write("Comment=runomatic autostart\n")
-		f.write("Exec=/usr/bin/runomatic\n")
-		f.write("Terminal=false\n")
-		f.write("Type=Application\n")
-elif os.path.isfile("/etc/xdg/autostart/runomatic.desktop"):
-	os.remove("/etc/xdg/autostart/runomatic.desktop")
-		
+	os.execv("%s/runoconfig.py"%self.baseDir,["1"])
