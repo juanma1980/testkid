fdfd67aa3eeab27242b941eaf130ca49f657550a
lliurex <juanma1980@gmail.com>
2022-07-14 11:53:35 +0200

    Fix user config path

--- src/runoconfig.py
+++ src/runoconfig.py
@@ -9,7 +9,7 @@ config.setRsrcPath("/usr/share/runomatic/rsrc")
 config.setIcon('runomatic.svg')
 config.setBanner('banner.png')
 config.setBackgroundImage('background.png')
-config.setConfig(confDirs={'system':'/usr/share/runomatic','user':'%s/.config'%os.environ['HOME']},confFile="runomatic.conf")
+config.setConfig(confDirs={'system':'/usr/share/runomatic','user':'%s/.config/runomatic'%os.environ['HOME']},confFile="runomatic.conf")
 config.Show()
 config.setFixedSize(config.width(),config.height())
 
